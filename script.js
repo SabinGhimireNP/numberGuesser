615ac9432af21c5e7989b8f62613eb3ea72d59ad
Sabin Ghimire <sabinghimire223@gmail.com>
2025-02-07 09:50:15 +0545

    Almost all function are added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numberGenerated = Math.floor(Math.random() * 100 + 1);
+const numberGenerated = parseInt(Math.floor(Math.random() * 100 + 1));
 console.log(numberGenerated);
 const lowOrHI_div = document.querySelector(".lowOrHi");
 let guesses = 10;
@@ -11,6 +11,12 @@ form.addEventListener("submit", function (event) {
 
   do {
     let numberGussed = parseInt(document.getElementById("guessField").value);
+    let diference_value = difference(numberGenerated, numberGussed);
+    function difference(a, b) {
+      return a - b;
+      // console.log(Math.abs(a - b));
+    }
+    let diference_value_abs = Math.abs(diference_value);
     if (isNaN(numberGussed)) {
       alert("Please, Enter a Number");
       return;
@@ -23,13 +29,34 @@ form.addEventListener("submit", function (event) {
         lowOrHI_div.innerHTML = `<span>🎉Great job on guessing the number!🎉</span>`;
         lowOrHI_div.append;
         break;
-      } else if (numberGussed !== numberGenerated);
-      guesses--;
-      previousValue.textContent = `${numberGussed}`;
-      guess_holder.textContent = `${guesses}`;
-      form.reset();
-      if (!(guesses > 0)) {
-        alert("GAME OVER!!!");
+      } else if (numberGussed !== numberGenerated) {
+        guesses--;
+        previousValue.textContent = `${numberGussed}`;
+        guess_holder.textContent = `${guesses}`;
+        if (diference_value_abs <= 15) 
+          {
+          if (diference_value < 0) 
+          {
+            lowOrHI_div.innerHTML = `<span>Almost there! 🤏 So close! Just a little lower! ⬇️</span>`;
+          }
+        else if (diference_value > 0){
+          lowOrHI_div.innerHTML = `<span>Almost there! 🤩 So close! Just a little higher! ⬆️</span>`;
+        }
+        }
+        if (diference_value_abs >= 15) 
+          {
+          if (diference_value < 0) 
+          {
+            lowOrHI_div.innerHTML = `<span>Oops! Too low! 🤔 Try a bit higher! ⬆️</span>`;
+          }
+        else if (diference_value > 0){
+          lowOrHI_div.innerHTML = `<span>Oops! Too high! 😅 Try a bit lower! ⬇️</span>`;
+        }
+        }
+        form.reset();
+        if (!(guesses > 0)) {
+          alert("GAME OVER!!!");
+        }
       }
     }
   } while (outputMatched === false);
